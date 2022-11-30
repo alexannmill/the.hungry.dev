a4a08ebe691dcdb8149d86b7626c50ebad417409
Alex Miller <alexannmill@gmail.com>
2022-11-30 12:49:29 -0800

    changing to x, y coords

--- app.js
+++ app.js
@@ -1,30 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
   // --- Set grid size
-  const width = 25;
+  const width = 52;
+  const height = 7;
   // ---- Grabbing elements from the DOM
-  const playingField = document.getElementById("playing_field");
-  // const ctx = canvas.getContext("2d");
+  const playingField = document.querySelector(".playing_field");
   const scoreDsp = document.querySelector("span");
   const startBtn = document.getElementById("start_btn");
 
-
   // ---- Building playing field/grid
-  for (let i = 0; i < width; i++) {
-    for (let x = 0; x < width; x++) {
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
       const innerDiv = document.createElement("div");
-      playingField[0].appendChild(innerDiv);
+      playingField.appendChild(innerDiv);
     }
-    const next = document.createElement("br");
-    playingField[0].appendChild(next);
   }
-  const positions = document.querySelectorAll(".playing_field div");
-  console.log("positions.length:", positions.length);
+  const days = document.querySelectorAll(".playing_field div");
+  console.log("days.length:", days.length);
 
   // ---- Setting initial index
   let currentInd = 0;
-  let foodInd = 0;
+  let commitInd = 0;
   let snake = [2, 1, 0];
-  let direction = 1;
+  let x = 2;
+  let y = 6;
   let score = 0;
   let speed = 0.9;
   let intervalTime = 0;
@@ -33,28 +31,29 @@ document.addEventListener("DOMContentLoaded", () => {
   // ---- Starting the game
   const start = (e) => {
     reset();
-    randomFood();
-    direction = 1;
+    randomCommit();
+    x = 2;
+    y = 6;
     scoreDsp.innerText = score;
     intervalTime = 1000;
     snake = [2, 1, 0];
     currentInd = 0;
     snake.forEach((i) => {
-      positions[i].classList.add("snake");
+      days[i].classList.add("snake");
     });
     interval = setInterval(outcome, intervalTime);
   };
 
   const addSnakeClass = (position) => {
-    return positions[position].classList.add("snake");
+    return days[position].classList.add("snake");
   };
   const removeSnakeClass = (position) => {
-    return positions[position].classList.remove("snake");
+    return days[position].classList.remove("snake");
   };
   // ---- Reset snake, food, score and and interval
   const reset = () => {
     snake.forEach((i) => removeSnakeClass(i));
-    positions[foodInd].classList.remove("food");
+    days[commitInd].classList.remove("commit");
     clearInterval(interval);
     score = 0;
   };
@@ -64,24 +63,25 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("snake:", snake);
     const head = snake[0];
     const tail = snake.pop();
-
+    console.log("width:", width);
+    console.log("direction:", direction);
     if (
       //snake hitting border;  top , bottom, left, right
-      (head + width >= width * width && direction === width) ||
-      (head - width <= 0 && direction === -width) ||
+      (head + height >= width * width && direction === width) ||
+      (head - height <= 0 && direction === -width) ||
       (head % width === 0 && direction === 1) ||
       (head % width === width - 1 && direction === -1) ||
       //hitting itself
-      positions[head + direction].classList.contains("snake")
+      days[head + direction].classList.contains("snake")
     ) {
       return clearInterval(interval);
     }
 
-    if (positions[head + direction].classList.contains("food")) {
-      positions[head].classList.remove("apple");
+    if (days[head + direction].classList.contains("commit")) {
+      days[head].classList.remove("apple");
       addSnakeClass(tail);
       snake.push(tail);
-      randomFood();
+      randomCommit();
       score++;
       scoreDsp.textContent(score);
       clearInterval(interval);
@@ -99,22 +99,22 @@ document.addEventListener("DOMContentLoaded", () => {
     switch (e.keyCode) {
       // up
       case 38:
-        direction = -width;
+        y += 1;
         break;
 
       // down
       case 40:
-        direction = +width;
+        y -= 1;
         break;
 
       // left
       case 37:
-        direction = -1;
+        x -= 1;
         break;
 
       // right
       case 39:
-        direction = 1;
+        x += 1;
         break;
 
       default:
@@ -122,14 +122,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   };
 
-  const randomFood = () => {
-    foodInd = Math.floor(Math.random() * positions.length);
-    if (snake.includes(foodInd)) {
-      randomFood();
+  const randomCommit = () => {
+    commitInd = Math.floor(Math.random() * days.length);
+    if (snake.includes(commitInd)) {
+      randomCommit();
     }
-    console.log("foodInd:", foodInd);
-    positions[foodInd].classList.add("food");
-    console.log("position[foodInd]:", positions[foodInd]);
+    console.log("commitInd:", commitInd);
+    days[commitInd].classList.add("commit");
+    console.log("position[commitInd]:", days[commitInd]);
   };
 
   // ------------------add alert and colour changes
@@ -138,5 +138,5 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   document.addEventListener("keyup", movement);
-  document.addEventListener("click", start);
+  startBtn.addEventListener("click", start);
 });
